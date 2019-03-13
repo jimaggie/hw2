8c9468ba82eaafa0e1ef49cc69e7df5de7a95aae
mkshah98 <46585928+mkshah98@users.noreply.github.com>
2019-03-12 18:23:20 -0700

    Update ConstReverseDoublyLinkedListIterator.h

--- ConstReverseDoublyLinkedListIterator.h
+++ ConstReverseDoublyLinkedListIterator.h
@@ -4,6 +4,14 @@
 
 #ifndef TESTDOUBLELINKEDLIST_CONSTREVERSEDOUBLYLINKEDLISTITERATOR_H
 #define TESTDOUBLELINKEDLIST_CONSTREVERSEDOUBLYLINKEDLISTITERATOR_H
+#include "DoubleLinkedNode.h"
+#include "DoublyLinkedList.h"
+
+template <typename T>
+class DoublyLinkedList;
+
+template <typename T>
+class DoubleLinkedNode;
 
 template<typename T>
 class ConstReverseDoublyLinkedListIterator {
@@ -13,7 +21,7 @@ class ConstReverseDoublyLinkedListIterator {
 
   //copy constructor
   ConstReverseDoublyLinkedListIterator(const ConstReverseDoublyLinkedListIterator& orig);
-
+  ConstReverseDoublyLinkedListIterator(const ConstReverseDoublyLinkedListIterator& orig_list, DoubleLinkedNode<T>* start);
   //are the two iterators equal?
   //they are if they are over the same doubly linked list
   //and (they are referring to the same element in the list
@@ -37,6 +45,50 @@ class ConstReverseDoublyLinkedListIterator {
   //get a const reference to the value
   const T& operator*() const;
 
+protected:
+    ConstReverseDoublyLinkedListIterator<T> rev;
+    DoubleLinkedNode* current;
 };
 
+template <typename T>
+ConstReverseDoublyLinkedListIterator::ConstReverseDoublyLinkedListIterator(const ConstReverseDoublyLinkedListIterator &orig): rev(orig){}
+
+ConstReverseDoublyLinkedListIterator::ConstReverseDoublyLinkedListIterator(const ConstReverseDoublyLinkedListIterator &orig_list, DoubleLinkedNode<T> *start):rev(orig_list), current(start){}
+
+bool ConstReverseDoublyLinkedListIterator::operator==(const ConstReverseDoublyLinkedListIterator<T> &rhs) const {
+    return rhs == this;
+}
+
+bool ConstReverseDoublyLinkedListIterator::operator!=(ConstReverseDoublyLinkedListIterator<T> &rhs) const {
+    return rhs != this;
+}
+
+template <typename T>
+bool ConstReverseDoublyLinkedListIterator::operator bool() const {
+    return current != nullptr;
+}
+
+template <typename T>
+const T& ConstReverseDoublyLinkedListIterator::operator*() const {
+    return current->value;
+}
+
+template <typename T>
+const ConstReverseDoublyLinkedListIterator<T> ConstReverseDoublyLinkedListIterator::operator++(int) {
+    current->prev = current;
+    return current;
+}
+
+template <typename T>
+ConstReverseDoublyLinkedListIterator<T> & ConstReverseDoublyLinkedListIterator::operator++() {
+    current = current->prev;
+    return current;
+}
+
+template <typename T>
+const ConstReverseDoublyLinkedListIterator<T> ConstReverseDoublyLinkedListIterator::operator--(int) {
+    ConstReverseDoublyLinkedListIterator<T> cur(*this);
+    cur.current->prev = current;
+    return cur;
+}
 #endif //TESTDOUBLELINKEDLIST_CONSTREVERSEDOUBLYLINKEDLISTITERATOR_H
