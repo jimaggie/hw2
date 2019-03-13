0baef8523673e7bb98725ecf6a1fe4b6ec6eb70f
mkshah98 <46585928+mkshah98@users.noreply.github.com>
2019-03-12 18:22:20 -0700

    Update ConstDoublyLinkedListIterator.h

--- ConstDoublyLinkedListIterator.h
+++ ConstDoublyLinkedListIterator.h
@@ -1,59 +1,105 @@
 //
-// Created by mfbut on 2/16/2019.
+// Created by mfbut on 2/11/2019.
 //
 
-#ifndef TESTDOUBLELINKEDLIST_CONSTDOUBLYLINKEDLISTITERATOR_H
-#define TESTDOUBLELINKEDLIST_CONSTDOUBLYLINKEDLISTITERATOR_H
+#ifndef LINKEDLIST_GENERICDOUBLYLINKEDLISTITERATOR_H
+#define LINKEDLIST_GENERICDOUBLYLINKEDLISTITERATOR_H
+
+#include "DoubleLinkedNode.h"
+#include "DoublyLinkedList.h"
+template <typename T>
+class DoublyLinkedListNode;
 
 template<typename T>
-class ConstDoublyLinkedListIterator {
-//you must implement at least the methods below
+class DoublyLinkedListIterator {
+  //you must implement at least the methods below
   //you are free to add (and will likely need to add)
   //more members and methods
 
+private:
+    T pos;
+    ConstLinkedListIterator &fwd;
+
  public:
-  ConstDoublyLinkedListIterator(const ConstDoublyLinkedListIterator& orig);
+  DoublyLinkedListIterator(const DoublyLinkedListIterator& orig);
+  DoublyLinkedListIterator(const DoublyLinkedListIterator &list1, )
   //are the two iterators equal?
   //they are if they are over the same doubly linked list
   //and (they are referring to the same element in the list
   //or they are out of bounds)
-  bool operator==(const ConstDoublyLinkedListIterator<T>& rhs) const;
+  bool operator==(const DoublyLinkedListIterator<T>& rhs) const;
 
   //are the two iterators different?
-  bool operator!=(const ConstDoublyLinkedListIterator<T>& rhs) const;
+  bool operator!=(const DoublyLinkedListIterator<T>& rhs) const;
 
   //is the iterator safe to dereference?
   operator bool() const;
 
   //go to the next element
-  ConstDoublyLinkedListIterator<T>& operator++(); //pre
-  const ConstDoublyLinkedListIterator<T> operator++(int);//post
+  DoublyLinkedListIterator<T>& operator++(); //pre
+  const DoublyLinkedListIterator<T> operator++(int& amt);//post
 
   //go to the previous element
-  ConstDoublyLinkedListIterator<T>& operator--(); //pre
-  const ConstDoublyLinkedListIterator<T> operator--(int); //post
+  DoublyLinkedListIterator<T>& operator--(); //pre
+  const DoublyLinkedListIterator<T> operator--(int& amt); //post
 
   //get a const reference to the value
   const T& operator*() const;
 
+  //get a non const iterator
+  T& operator*();
+
+private:
+    DoublyLinkedListIterator<T> &fwd;
+    DoublyLinkedListNode &list_node;
+
 };
 
-bool operator==(const ConstDoublyLinkedListIterator<T>& rhs) const {
-    if (rhs == this){
-        return true;
-    }
-    else{
-        return false;
-    }
+DoublyLinkedListIterator::DoublyLinkedListIterator(const DoublyLinkedListIterator &orig): fwd(orig) {}
+
+const T & DoublyLinkedListIterator::operator*() const {
+  return list_node->value;
 }
 
-bool operator!=(const ConstDoublyLinkedListIterator<T>& rhs)const {
-    if(rhs != this){
-        return true;
-    }
-    else{
-        return false;
-    }
+T & DoublyLinkedListIterator::operator*() {
+  return list_node.value;
 }
 
-#endif //TESTDOUBLELINKEDLIST_CONSTDOUBLYLINKEDLISTITERATOR_H
+bool DoublyLinkedListIterator::operator bool() const {
+  return *this != nullptr;
+}
+
+bool DoublyLinkedListIterator::operator==(const DoublyLinkedListIterator<T> &rhs) const {
+  return rhs == *this;
+}
+
+bool DoublyLinkedListIterator::operator!=(const DoublyLinkedListIterator<T> &rhs) const {
+  return rhs != *this;
+}
+
+DoublyLinkedListIterator<T> & DoublyLinkedListIterator::operator++(int) {
+    DoublyLinkedListIterator copy(*this);
+    ++(*this);
+  return copy;
+}
+
+DoublyLinkedListIterator<T> & DoublyLinkedListIterator::operator++() {
+  ;
+  return *this;
+}
+
+const DoublyLinkedListIterator<T> DoublyLinkedListIterator::operator--(int) {
+  DoublyLinkedListIterator copy(*this);
+  --(*this);
+  return copy;
+}
+
+const T & DoublyLinkedListIterator::operator*() const {
+    return list_node.value;
+}
+
+T & DoublyLinkedListIterator::operator*() {
+    return list_node.value;
+}
+
+#endif //LINKEDLIST_GENERICDOUBLYLINKEDLISTITERATOR_H
